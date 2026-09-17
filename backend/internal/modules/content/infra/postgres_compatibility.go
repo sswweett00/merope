c8fdb791c657b29e02c0feb2d1195d817fec3592
kaanaydinli <iamkaankaan@gmail.com>
2026-09-17 14:01:08 +0300

    fix: implement postgres content node deletion

---
+++ backend/internal/modules/content/infra/postgres_compatibility.go
@@ -0,0 +1,21 @@
+package infra
+
+import (
+	"context"
+	"fmt"
+
+	"local/merope/internal/core/util"
+	"local/merope/internal/modules/content/domain"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func (r *PostgresContentRepository) DeleteNode(ctx context.Context, nodeID string) error {
+	var id pgtype.UUID
+	if err := id.Scan(nodeID); err != nil {
+		return fmt.Errorf("invalid node uuid: %w", err)
+	}
+	return r.queries.DeleteComment(ctx, id)
+}
+
+var _ domain.ContentRepository = (*PostgresContentRepository)(nil)
+var _ = util.UUIDToString
